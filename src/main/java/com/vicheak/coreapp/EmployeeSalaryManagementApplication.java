f8e6e272b3690da9ba082b7a4e240f7ed1b5c65f
Vicheak <suonvicheak991@gmail.com>
2023-11-14 10:38:10 +0700

    Init Project

---
+++ src/main/java/com/vicheak/coreapp/EmployeeSalaryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.vicheak.coreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSalaryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSalaryManagementApplication.class, args);
+    }
+
+}
