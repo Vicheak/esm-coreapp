b3e615c7da61b3d6e750e90f00e37a875aa6d028
Vicheak <suonvicheak991@gmail.com>
2023-12-10 14:12:28 +0700

    update project

--- src/main/java/com/vicheak/coreapp/api/slip/SalaryPaymentGross.java
+++ src/main/java/com/vicheak/coreapp/api/slip/SalaryPaymentGross.java
@@ -2,10 +2,7 @@ package com.vicheak.coreapp.api.slip;
 
 import com.vicheak.coreapp.api.salarygross.SalaryGross;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,6 +10,8 @@ import java.math.BigDecimal;
 @Setter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "salary_payment_gross")
 public class SalaryPaymentGross {
