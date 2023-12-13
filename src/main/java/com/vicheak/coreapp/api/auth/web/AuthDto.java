eeb3e8edb6f6550ea20c83fac6657b9f3d95de30
Vicheak <suonvicheak991@gmail.com>
2023-12-13 22:12:52 +0700

    implement jwt

---
+++ src/main/java/com/vicheak/coreapp/api/auth/web/AuthDto.java
@@ -0,0 +1,11 @@
+package com.vicheak.coreapp.api.auth.web;
+
+import lombok.Builder;
+
+@Builder
+public record AuthDto(String accessToken,
+
+                      String refreshToken,
+
+                      String type) {
+}
